4b9643f35bac41d6cf7a6f9c949c36a5d4b3ef12
Felipe B. de Moura <fittato@gmail.com>
2023-03-31 13:32:49 -0300

    Fazendo cadastro de produtos. Página 211

---
+++ CSharp-Start/MinhasClasses/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpStart.MinhasClasses
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Preco { get; set;}
+    }
+}
