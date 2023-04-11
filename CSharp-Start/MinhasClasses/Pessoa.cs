9ad5c92312d150c35d8f7760f8cb7aa672e17a66
Felipe B. de Moura <fittato@gmail.com>
2023-04-11 13:28:11 -0300

    Continuar a partidar da página 222, com o código @page '/pessoacadastra'

---
+++ CSharp-Start/MinhasClasses/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpStart.MinhasClasses
+{
+    public static class Pessoa
+    {
+        public static string Nome { get; set; }
+        public static string Endereco { get; set; }
+        public static int Idade { get; set; }
+
+    }
+}
